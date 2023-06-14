40dfe6621e922d658e521a7bae5ea72c8b64d692
wassem7 <waseemdarkwa@gmail.com>
2023-06-14 14:16:11 +0100

    Sticky Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,9 @@ import { type NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <>
-      <h1>Hi</h1>
+      <header className="sticky top-0 z-10 border-b bg-white pt-2">
+        <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
+      </header>
     </>
   );
 };
